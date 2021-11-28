42f14c5ea216551d0aac305693a8fb192b44147b
NHBaoMinh <nguyenhabaominh162@gmail.com>
2021-11-28 14:59:28 +0700

    Fix Signin Signup Header

--- src/public/js/user/signin.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-    console.log('object');
-    document.getElementById('model').setAttribute('class', 'model-show');
-}
-document.getElementById('myButton').addEventListener('click', myFunction);
