ad3cf751439448150187c7fb23f03f459f11254c
hlphap <longphappy@gmail.com>
2021-12-08 00:00:50 +0700

    first

--- src/app/controllers/home.controller.js
+++ src/app/controllers/home.controller.js
@@ -9,7 +9,7 @@ const index = catchAsync(async (req, res) => {
     // Select view to render
     return res.render('user/home', {
         title: 'Home', //  Required Title
-        styles: ['layout-user', 'header', 'footer'], // Required Stylesheet name from public
+        styles: ['layout-user', 'header', 'footer', 'home'], // Required Stylesheet name from public
         scripts: [], // Required Script name from public
         data, // Data to handlebars
     });
