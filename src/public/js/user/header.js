06c049611c85b61206afa13d443edfcff1360cd7
hlphap <longphappy@gmail.com>
2021-12-08 00:59:30 +0700

    blog

--- src/public/js/user/header.js
+++ src/public/js/user/header.js
@@ -1,61 +1,61 @@
-// const loginBtn = document.getElementById('loginBtn');
-// const signupBtn = document.getElementById('registryBtn');
-// const loginForm = document.getElementById('login-form');
-// let mouseInside;
-// const signupForm = document.getElementById('signup-form');
-// const btnLogout = document.getElementById('btnLogout');
+const loginBtn = document.getElementById('loginBtn');
+const signupBtn = document.getElementById('registryBtn');
+const loginForm = document.getElementById('login-form');
+let mouseInside;
+const signupForm = document.getElementById('signup-form');
+const btnLogout = document.getElementById('btnLogout');
 
-// if (loginBtn) {
-//     loginBtn.onclick = function () {
-//         loginForm.style.display = 'flex';
-//         loginForm.style.right = '725px';
-//     };
-// }
+if (loginBtn) {
+    loginBtn.onclick = function () {
+        loginForm.style.display = 'flex';
+        loginForm.style.right = '725px';
+    };
+}
 
-// if (loginForm) {
-//     loginForm.onmouseover = function () {
-//         mouseInside = true;
-//     };
+if (loginForm) {
+    loginForm.onmouseover = function () {
+        mouseInside = true;
+    };
 
-//     loginForm.onmouseout = function () {
-//         mouseInside = false;
-//     };
-// }
+    loginForm.onmouseout = function () {
+        mouseInside = false;
+    };
+}
 
-// document.querySelector('body').onmouseup = function () {
-//     if (!mouseInside) {
-//         loginForm.style.display = 'none';
-//         signupForm.style.display = 'none';
-//     }
-// };
-// if (signupBtn) {
-//     signupBtn.onclick = function () {
-//         signupForm.style.display = 'flex';
-//         signupForm.style.right = '815px';
-//     };
-// }
+document.querySelector('body').onmouseup = function () {
+    if (!mouseInside) {
+        loginForm.style.display = 'none';
+        signupForm.style.display = 'none';
+    }
+};
+if (signupBtn) {
+    signupBtn.onclick = function () {
+        signupForm.style.display = 'flex';
+        signupForm.style.right = '815px';
+    };
+}
 
-// if (signupForm) {
-//     signupForm.onmouseover = function () {
-//         mouseInside = true;
-//     };
+if (signupForm) {
+    signupForm.onmouseover = function () {
+        mouseInside = true;
+    };
 
-//     signupForm.onmouseout = function () {
-//         mouseInside = false;
-//     };
-// }
+    signupForm.onmouseout = function () {
+        mouseInside = false;
+    };
+}
 
-// if (btnLogout) {
-//     btnLogout.onclick = (event) => {
-//         event.preventDefault();
-//         axios
-//             .post('/auth/logout')
-//             .then((response) => {
-//                 console.log('object');
-//                 location.reload();
-//             })
-//             .catch((error) => {
-//                 console.error(error);
-//             });
-//     };
-// }
+if (btnLogout) {
+    btnLogout.onclick = (event) => {
+        event.preventDefault();
+        axios
+            .post('/auth/logout')
+            .then((response) => {
+                console.log('object');
+                location.reload();
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+}
